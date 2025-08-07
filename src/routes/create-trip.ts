7b1228400deaf59e608f30ff95eb77d834c94e4b
danichagas <chagasdaniel788@gmail.com>
2025-08-07 16:13:01 -0300

    feat: initializing the trip creation route

---
+++ src/routes/create-trip.ts
@@ -0,0 +1,12 @@
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+
+export async function createTrip(app: FastifyInstance) {
+    app.withTypeProvider<ZodTypeProvider>().post('/trips', {
+        schema: {
+            
+        }
+    },async () => {
+        return 'Hello World'
+    })
+}
