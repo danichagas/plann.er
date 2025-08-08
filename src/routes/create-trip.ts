7f2bb52137a31408cd5e91ae9bf9324633acd7ae
danichagas <chagasdaniel788@gmail.com>
2025-08-08 16:13:38 -0300

    feat: validating start and end dates

--- src/routes/create-trip.ts
+++ src/routes/create-trip.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from 'fastify'
 import { ZodTypeProvider } from 'fastify-type-provider-zod'
 import { z } from 'zod'
 import { prisma } from '../lib/prisma'
+import dayjs from 'dayjs'
 
 export async function createTrip(app: FastifyInstance) {
     app.withTypeProvider<ZodTypeProvider>().post('/trips', {
@@ -10,10 +11,20 @@ export async function createTrip(app: FastifyInstance) {
                 destination: z.string().min(4),
                 starts_at: z.coerce.date(),
                 ends_at: z.coerce.date(),
+                owner_name: z.string(),
+                owner_email: z.email()
             })
         }
     },async (request) => {
-        const { destination, starts_at, ends_at } = request.body
+        const { destination, starts_at, ends_at, owner_name, owner_email } = request.body
+
+        if(dayjs(starts_at).isBefore(new Date())) {
+            throw new Error('Invalid trip start date.')
+        }
+
+        if(dayjs(ends_at).isBefore(starts_at)) {
+            throw new Error('Invalid trip end date')
+        }
 
         const trip = await prisma.trip.create({
             data: {
